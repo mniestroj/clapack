d8da6d99e4f46b60fbc61ffe102e43dfd8aa6a2f
Marcin Niestroj <macius1990w@gmail.com>
2015-05-31 11:20:18 +0200

    f2c: Changed implementation of c_sqrt(complex *, complex *) from double to float

--- F2CLIBS/libf2c/c_sqrt.c
+++ F2CLIBS/libf2c/c_sqrt.c
@@ -1,7 +1,7 @@
 #include "f2c.h"
 
 #ifdef KR_headers
-extern double sqrt(), f__cabs();
+extern float sqrtf(), f__cabsf();
 
 VOID c_sqrt(r, z) complex *r, *z;
 #else
@@ -10,30 +10,30 @@ VOID c_sqrt(r, z) complex *r, *z;
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern double f__cabs(double, double);
+extern float f__cabsf(float, float);
 
 void c_sqrt(complex *r, complex *z)
 #endif
 {
-	double mag, t;
-	double zi = z->i, zr = z->r;
+	float mag, t;
+	float zi = z->i, zr = z->r;
 
-	if( (mag = f__cabs(zr, zi)) == 0.)
-		r->r = r->i = 0.;
+	if( (mag = f__cabsf(zr, zi)) == 0.f)
+		r->r = r->i = 0.f;
 	else if(zr > 0)
 		{
-		r->r = t = sqrt(0.5 * (mag + zr) );
+		r->r = t = sqrtf(0.5f * (mag + zr) );
 		t = zi / t;
-		r->i = 0.5 * t;
+		r->i = 0.5f * t;
 		}
 	else
 		{
-		t = sqrt(0.5 * (mag - zr) );
+		t = sqrt(0.5f * (mag - zr) );
 		if(zi < 0)
 			t = -t;
 		r->i = t;
 		t = zi / t;
-		r->r = 0.5 * t;
+		r->r = 0.5f * t;
 		}
 	}
 #ifdef __cplusplus
